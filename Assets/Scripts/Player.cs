d716864ff4ae411eea301e85949702be1e21b261
Ethan Munday <ethanmunday80@googlemail.com>
2022-12-09 17:57:09 +0000

    Movement fully coded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,7 @@ using UnityEngine.Rendering;
 
 public class PlayerState
 {
-    public virtual void handleInput(Player thisObj) { }
-
-    public virtual void report(Player thisObj) { }
-
-}
-
-public class GroundedState : PlayerState
-{
-    public override void handleInput(Player thisObj)
+    public virtual void handleInput(Player thisObj) 
     {
         if (Input.GetKey(KeyCode.D))
         {
@@ -24,6 +16,7 @@ public class GroundedState : PlayerState
                 thisObj.xInput = 0.0f;
             }
         }
+
         else if (Input.GetKey(KeyCode.A))
         {
             thisObj.xInput = -1.0f;
@@ -37,115 +30,260 @@ public class GroundedState : PlayerState
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
         {
             thisObj.yInput = 1.0f;
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                thisObj.yInput = 0.0f;
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            thisObj.yInput = -1.0f;
         }
 
         else
         {
             thisObj.yInput = 0.0f;
         }
-        thisObj.controller.MoveGround();
+
+        thisObj.jumpPressed = Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.Space);
+    }
+
+    public virtual void caclulateMovement(Player thisObj) { }
+
+    public virtual void report(Player thisObj) { }
+
+    public virtual void detectHit(Player thisObj, Collision2D collision) { }
+    public virtual void exitHit(Player thisObj, Collision2D collision) { }
+
+}
+
+public class GroundedState : PlayerState
+{
+    public override void caclulateMovement(Player thisObj)
+    {
+        if (thisObj.isTouchingWall && thisObj.jumpPressed && thisObj.wallClimbCount > 0)
+        {
+            Debug.Log("Swapped from Grounded to Climbing");
+            thisObj.currentWallClimbCount -= 1;
+            thisObj.currentState = new ClimbingState();
+        }
 
         if (!thisObj.controller.isGrounded)
         {
             Debug.Log("Swapped from Grounded to Airborne");
             thisObj.currentState = new AirborneState();
         }
-
+        
+        if (thisObj.currentState.ToString() == "GroundedState")
+        {
+            thisObj.controller.MoveGround();
+        }
     }
+
     public override void report(Player thisObj)
     {
         Debug.Log("Grounded");
     }
+
+    public override void detectHit(Player thisObj, Collision2D collision)
+    {
+        if (thisObj.controller.GroundedCheck(collision.collider) && thisObj.controller.WallCheck(collision))
+        {
+            thisObj.controller.AutoStep(thisObj, collision);
+            thisObj.isTouchingWall = true;
+            thisObj.currentWall = collision.gameObject;
+            thisObj.wallDirection = collision.GetContact(0).normal;
+        }
+    }
+
+    public override void exitHit(Player thisObj, Collision2D collision)
+    {
+        if (collision.gameObject == thisObj.currentWall)
+        {
+            thisObj.isTouchingWall = false;
+        }
+    }
 }
+
+public class LandingState : GroundedState
+{
+    float landTimer = 0.3f;
+    public override void caclulateMovement(Player thisObj)
+    {
+        landTimer -= Time.deltaTime;
+
+        if (landTimer <= 0)
+        {
+            Debug.Log("Swapped from Landing to Grounded");
+            thisObj.currentState = new GroundedState();
+        }
+
+        if (thisObj.isTouchingWall && thisObj.jumpPressed && thisObj.wallClimbCount > 0)
+        {
+            Debug.Log("Swapped from Landing to Climbing");
+            thisObj.currentWallClimbCount -= 1;
+            thisObj.currentState = new ClimbingState();
+        }
+
+        if (!thisObj.controller.isGrounded)
+        {
+            Debug.Log("Swapped from Landing to Airborne");
+            thisObj.rb.velocity = new Vector2(thisObj.rb.velocity.x * 2f, thisObj.rb.velocity.y);
+            thisObj.currentState = new AirborneState();
+        }
+
+        if (thisObj.currentState.ToString() == "LandingState")
+        {
+            thisObj.controller.MoveGround();
+        }
+    }
+
+    public override void report(Player thisObj)
+    {
+        Debug.Log("Landing");
+    }
+}
+
 
 public class AirborneState : PlayerState
 {
-    public override void handleInput(Player thisObj)
+    bool isOnFloor = false;
+    float launchInput = 0.0f;
+
+    public override void caclulateMovement(Player thisObj)
     {
-        if (Input.GetKey(KeyCode.D))
+        if (thisObj.isTouchingWall && thisObj.jumpPressed && thisObj.currentWallClimbCount > 0)
         {
-            thisObj.xInput = 1.0f;
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                thisObj.xInput = 0.0f;
-            }
+            Debug.Log("Swapped from Airborne to Climbing");
+            thisObj.currentWallClimbCount -= 1;
+            thisObj.currentState = new ClimbingState();
         }
 
-        else if (Input.GetKey(KeyCode.A))
+        if (isOnFloor && thisObj.controller.isGrounded)
         {
-            thisObj.xInput = -1.0f;
-        }
-
-        else
-        {
-            thisObj.xInput = 0.0f;
-        }
-
-        if (thisObj.controller.isGrounded)
-        {
-            Debug.Log("Swapped from Airborne to Grounded");
-            thisObj.currentState = new GroundedState();
+            Debug.Log("Swapped from Airborne to Landing");
             thisObj.currentJumpCount = thisObj.jumpCount;
+            thisObj.currentDashCount = thisObj.dashCount;
+            thisObj.currentWallClimbCount = thisObj.wallClimbCount;
+            thisObj.currentState = new LandingState();
         }
 
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && thisObj.currentJumpCount > 0)
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            Debug.Log("Swapped from Airborne to Flying");
-            thisObj.currentState = new FlyingState();
-            thisObj.currentJumpCount -= 1;
+            thisObj.controller.Dash();
         }
 
-        thisObj.controller.MoveAir();
+        if (thisObj.currentState.ToString() == "AirborneState")
+        {
+            thisObj.controller.MoveAir();
+        }
     }
 
     public override void report(Player thisObj)
     {
         Debug.Log("Airborne");
     }
+
+    public override void detectHit(Player thisObj, Collision2D collision)
+    {
+        if (thisObj.controller.GroundedCheck(collision.collider) && thisObj.controller.WallCheck(collision))
+        {
+            thisObj.isTouchingWall = true;
+            thisObj.currentWall = collision.gameObject;
+            thisObj.wallDirection = collision.GetContact(0).normal;
+        }
+
+        if (thisObj.controller.FloorCheck(collision))
+        {
+            isOnFloor = true;
+        }
+        
+    }
+
+    public override void exitHit(Player thisObj, Collision2D collision)
+    {
+        if (collision.gameObject == thisObj.currentWall)
+        {
+            thisObj.isTouchingWall = false;
+            isOnFloor = false;
+        }
+    }
 }
 
-public class FlyingState : PlayerState
+//Base Flying State
+public class ClimbingState : PlayerState
 {
-    float flytimer = 1.0f;
-
-    public override void handleInput(Player thisObj)
+    float flyTimer;
+    Vector2 direction;
+    public override void caclulateMovement(Player thisObj)
     {
-        if (Input.GetKey(KeyCode.D))
+        flyTimer -= Time.deltaTime;
+        if (flyTimer <= 0.0f)
         {
-            thisObj.xInput = 1.0f;
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                thisObj.xInput = 0.0f;
-            }
-        }
-
-        else if (Input.GetKey(KeyCode.A))
-        {
-            thisObj.xInput = -1.0f;
-        }
-
-        else
-        {
-            thisObj.xInput = 0.0f;
-        }
-
-        flytimer -= Time.deltaTime;
-        if (flytimer <= 0.0f)
-        {
-            Debug.Log("Swapped from Flying to Airborne");
+            Debug.Log("Swapped from Climbing to Airborne");
             thisObj.currentState = new AirborneState();
         }
 
-        thisObj.controller.LinearMovement(0.0f, 10.0f);
+        if (thisObj.xInput != 0 && Input.GetKeyDown(KeyCode.J) && thisObj.dashCount > 0)
+        {
+            Debug.Log("Swapped from Climbing to Airborne");
+            thisObj.controller.Dash(0.6f, 0.0f);
+            thisObj.currentState = new AirborneState();
+        }
+
+        if (!thisObj.isTouchingWall)
+        {
+            thisObj.rb.velocity = new Vector2(-thisObj.wallDirection.x * 80, 10f);
+            thisObj.currentState = new AirborneState();
+        }
+
+        if (thisObj.currentState.ToString() == "ClimbingState")
+        {
+            thisObj.controller.LinearMovement(direction);
+        }
     }
 
     public override void report(Player thisObj)
     {
-        Debug.Log("Flying");
+        Debug.Log("Climbing");
+    }
+
+    public override void detectHit(Player thisObj, Collision2D collision)
+    {
+        if (thisObj.controller.GroundedCheck(collision.collider) && thisObj.controller.WallCheck(collision))
+        {
+            thisObj.isTouchingWall = true;
+            thisObj.currentWall = collision.gameObject;
+        }
+    }
+
+    public override void exitHit(Player thisObj, Collision2D collision)
+    {
+        if (collision.gameObject == thisObj.currentWall)
+        {
+            thisObj.isTouchingWall = false;
+        }
+    }
+
+    public ClimbingState()
+    {
+        flyTimer = 0.3f;
+        direction = new Vector2(0.0f,30.0f);
+    }
+    public ClimbingState(float inputTimer)
+    {
+        flyTimer = inputTimer;
+        direction = new Vector2(0.0f, 30.0f);
+    }
+    public ClimbingState(float inputTimer, Vector2 inputDirection)
+    {
+        flyTimer = inputTimer;
+        direction = inputDirection;
     }
 }
+
 
 
 public class Player : MonoBehaviour
@@ -159,10 +297,19 @@ public class Player : MonoBehaviour
     //Details
     [HideInInspector] public float xInput;
     [HideInInspector] public float yInput;
+    [HideInInspector] public bool jumpPressed;
+    [HideInInspector] public bool isTouchingWall;
+    [HideInInspector] public GameObject currentWall;
+    [HideInInspector] public Vector2 wallDirection;
 
     //Customisables
     [HideInInspector] public float jumpCount;
     [HideInInspector] public float currentJumpCount;
+    [HideInInspector] public float dashCount;
+    [HideInInspector] public float currentDashCount;
+    [HideInInspector] public float wallClimbCount;
+    [HideInInspector] public float currentWallClimbCount;
+
 
     private void Awake()
     {
@@ -170,6 +317,11 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
         jumpCount = 1;
+        currentJumpCount = jumpCount;
+        dashCount = 1;
+        currentDashCount = dashCount;
+        wallClimbCount = 1;
+        currentWallClimbCount = wallClimbCount;
     }
 
     // Start is called before the first frame update
@@ -188,5 +340,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         currentState.handleInput(this);
+        currentState.caclulateMovement(this);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentState.detectHit(this, collision);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        currentState.exitHit(this, collision);
+    }
+
 }
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,53 +17,35 @@ public class PlayerController : MonoBehaviour
     public float airDrag;
     public float gravity;
     public LayerMask groundMask;
-
+    // Default Unity Messages
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider2D>();
         xVelocity = 0.0f;
         yVelocity = 0.0f;
+        this.GetComponent<Rigidbody2D>().gravityScale = gravity;
     }
-
+    private void Update()
+    {
+        GroundedCheck();
+    }
+    // Ground Movement
     public void MoveGround()
     {
         MoveGround(this.GetComponent<Player>());
     }
-
     public void MoveGround(Player thisObj)
     {
-        xVelocity = Mathf.Clamp(xVelocity + (thisObj.xInput * acceleration), -maxSpeed, maxSpeed);
-        if (thisObj.xInput == 0.0f || thisObj.xInput * xVelocity < 0)
+        xVelocity = (thisObj.rb.velocity.x + (thisObj.xInput * acceleration));
+        if (xVelocity > maxSpeed + 2 || xVelocity < -maxSpeed - 2)
         {
-            if (xVelocity > 0.0f)
-            {
-                xVelocity = Mathf.Clamp(xVelocity - groundDrag, 0.0f, maxSpeed);
-            }
-
-            else
-            {
-                xVelocity = Mathf.Clamp(xVelocity + groundDrag, -maxSpeed, 0.0f);
-            }
+            xVelocity *= 0.95f;
+        }
+        else
+        {
+            xVelocity = Mathf.Clamp(xVelocity, -maxSpeed, maxSpeed);
         }
 
-        if (isGrounded && thisObj.yInput == 1.0f)
-        {
-            thisObj.rb.velocity = (new Vector2(0, jumpHeight));
-        }
-        yVelocity = Mathf.Clamp(thisObj.rb.velocity.y, -jumpMaxSpeed, jumpMaxSpeed);
-
-        Vector2 newVelocity = new Vector2(xVelocity, yVelocity);
-        thisObj.rb.velocity = Vector2.SmoothDamp(thisObj.rb.velocity, newVelocity, ref velocityRef, Time.deltaTime);
-    }
-
-    public void MoveAir()
-    {
-        MoveAir(GetComponent<Player>());
-    }
-
-    public void MoveAir(Player thisObj)
-    {
-        xVelocity = Mathf.Clamp(xVelocity + (thisObj.xInput * acceleration), -maxSpeed, maxSpeed);
         if (thisObj.xInput == 0.0f || thisObj.xInput * xVelocity < 0)
         {
             if (xVelocity > 0.0f)
@@ -76,61 +58,191 @@ public class PlayerController : MonoBehaviour
                 xVelocity = Mathf.Clamp(xVelocity + airDrag, -maxSpeed, 0.0f);
             }
         }
-        yVelocity = Mathf.Clamp(thisObj.rb.velocity.y, -jumpMaxSpeed, jumpMaxSpeed);
+
+        if (isGrounded && thisObj.yInput == 1.0f)
+        {
+            Jump();
+        }
+
+        yVelocity = thisObj.rb.velocity.y;
+        if (yVelocity > maxSpeed + 2 || yVelocity < -maxSpeed - 2)
+        {
+            yVelocity *= 0.9f;
+        }
+        else
+        {
+            Mathf.Clamp(thisObj.rb.velocity.y, -jumpMaxSpeed, jumpMaxSpeed);
+        }
+
         Vector2 newVelocity = new Vector2(xVelocity, yVelocity);
         thisObj.rb.velocity = Vector2.SmoothDamp(thisObj.rb.velocity, newVelocity, ref velocityRef, Time.deltaTime);
     }
+    // Aerial Movement
+    public void MoveAir()
+    {
+        MoveAir(GetComponent<Player>());
+    }
+    public void MoveAir(Player thisObj)
+    {
+        xVelocity = (thisObj.rb.velocity.x + (thisObj.xInput * acceleration));
+        if (xVelocity > maxSpeed+2 || xVelocity < -maxSpeed-2)
+        {
+            xVelocity *= 0.95f;
+        }
+        else
+        {
+            xVelocity = Mathf.Clamp(xVelocity, -maxSpeed, maxSpeed);
+        }
 
+        if (thisObj.xInput == 0.0f || thisObj.xInput * xVelocity < 0)
+        {
+            if (xVelocity > 0.0f)
+            {
+                xVelocity = Mathf.Clamp(xVelocity - airDrag, 0.0f, maxSpeed);
+            }
+
+            else
+            {
+                xVelocity = Mathf.Clamp(xVelocity + airDrag, -maxSpeed, 0.0f);
+            }
+        }
+
+        if (thisObj.jumpPressed)
+        {
+            Jump(1.5f);
+        }
+
+        yVelocity = thisObj.rb.velocity.y;
+        if (yVelocity > maxSpeed + 2 || yVelocity < -maxSpeed - 2)
+        {
+            yVelocity *= 0.9f;
+        }
+        else
+        {
+            Mathf.Clamp(thisObj.rb.velocity.y, -jumpMaxSpeed, jumpMaxSpeed);
+        }
+        Vector2 newVelocity = new Vector2(xVelocity, yVelocity);
+        thisObj.rb.velocity = Vector2.SmoothDamp(thisObj.rb.velocity, newVelocity, ref velocityRef, Time.deltaTime);
+    }
+    // Jump
+    public void Jump()
+    {
+        Jump(1.0f, GetComponent<Player>());
+    }
+    public void Jump(float jumpMultiplier)
+    {
+        Jump(jumpMultiplier, GetComponent<Player>());
+    }
+    public void Jump(float jumpMultiplier, Player thisObj)
+    {
+        if (thisObj.currentJumpCount > 0)
+        {
+            thisObj.rb.velocity = (new Vector2(thisObj.rb.velocity.x, jumpHeight * jumpMultiplier));
+            thisObj.currentJumpCount -= 1;
+        }
+    }
+    // Dash
+    public void Dash()
+    {
+        Dash(0.6f, 0.6f, GetComponent<Player>());
+    }
+    public void Dash(float xScale, float yScale)
+    {
+        Dash(xScale, yScale, GetComponent<Player>());
+    }
+    public void Dash(float xScale, float yScale, Player thisObj)
+    {
+        if (thisObj.currentDashCount > 0)
+        {
+            thisObj.rb.AddForce(new Vector2(thisObj.xInput * xScale, thisObj.yInput * yScale));
+            thisObj.currentDashCount -= 1;
+        }
+    }
+    // Enable Gravity
     public void EnableGravity()
     {
         EnableGravity(GetComponent<Player>());
     }
-
     public void EnableGravity(float gravityChange)
     {
         EnableGravity(GetComponent<Player>(),gravityChange);
     }
-
     public void EnableGravity(Player thisObj)
     {
         thisObj.rb.gravityScale = gravity;
     }
-
     public void EnableGravity(Player thisObj, float gravityChange)
     {
         thisObj.rb.gravityScale = gravityChange;
     }
-
+    // Disable Gravity
     public void DisableGravity()
     {
         DisableGravity(GetComponent<Player>());
     }
-
     public void DisableGravity(Player thisObj)
     {
         thisObj.rb.gravityScale = 0;
     }
-
+    // Move Linearly in a Direction
     public void LinearMovement(float x, float y)
     {
         LinearMovement(x, y, GetComponent<Player>());
     }
-
+    public void LinearMovement(Vector2 direction)
+    {
+        LinearMovement(direction.x, direction.y, GetComponent<Player>());
+    }
     public void LinearMovement(float x, float y, Player thisObj)
     {
         Vector2 newVelocity = new Vector2(x, y);
         thisObj.rb.velocity = thisObj.rb.velocity = Vector2.SmoothDamp(thisObj.rb.velocity, newVelocity, ref velocityRef, Time.deltaTime);
     }
-
+    // Automatically Step Over Walls
+    public void AutoStep(Player thisObj, Collision2D collision)
+    {
+        float x = thisObj.transform.position.y - thisObj.collider.bounds.size.y / 2;
+        float y = collision.transform.position.y + collision.collider.bounds.size.y / 2;
+        if (y - x < 2.0f)
+        {
+            thisObj.rb.MovePosition(new Vector2(thisObj.rb.position.x, thisObj.rb.position.y + (y - x) + 0.1f));
+        }
+    }
+    public void AutoStep(Player thisObj, Collision2D collision, float sizeDifference)
+    {
+        float x = thisObj.transform.position.y - thisObj.collider.bounds.size.y / 2;
+        float y = collision.transform.position.y + collision.collider.bounds.size.y / 2;
+        if (y - x < sizeDifference)
+        {
+            thisObj.rb.MovePosition(new Vector2(thisObj.rb.position.x, thisObj.rb.position.y + (y - x) + 0.1f));
+        }
+    }
+    // Check if a layer is "Ground"
     private void GroundedCheck()
     {
         RaycastHit2D cast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down * 0.1f, 0.1f, groundMask);
         isGrounded = (cast.collider != null);
     }
-
-    // Update is called once per frame
-    void Update()
+    public bool GroundedCheck(Collider2D collider)
     {
-        GroundedCheck();
+        return collider.gameObject.layer == Mathf.Log(groundMask.value, 2.0f);
+    }
+    // Check if a collision happens with a wall
+    public bool WallCheck(Collision2D collision)
+    {
+        if (collision.contacts.Length != 0)
+        {
+            return Mathf.Approximately(collision.GetContact(0).normal.y, 0.0f);
+        }
+        return false;
+    }
+    // Check if a collision happens with the floor
+    public bool FloorCheck(Collision2D collision)
+    {
+        if (collision.contacts.Length != 0)
+        {
+            return Mathf.Approximately(collision.GetContact(0).normal.y, 1.0f);
+        }
+        return false;
     }
 }
